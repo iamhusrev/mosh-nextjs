956cfa4ba08c46b5b07bf5b85ae1757fa7780840
iamhusrev <iamhusrev@gmail.com>
2025-11-05 14:43:22 +0300

    Refactor ProductCard component; add AddToCart functionality and update styles

---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,11 @@
+import AddToCart from "./AddToCard";
+
+const ProductCard = () => {
+  return (
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="winter">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
