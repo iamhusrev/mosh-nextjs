1f8225ed2cf8a2127fb19487ffb91e7cf9cd679e
iamhusrev <iamhusrev@gmail.com>
2025-11-10 13:31:15 +0300

    Add NotFoundPage and UserNotFoundPage components with appropriate messages

---
+++ app/not-found.tsx
@@ -0,0 +1,6 @@
+
+const NotFoundPage = () => {
+  return <div>The requested page doesn&apos;t exist.</div>;
+};
+
+export default NotFoundPage;
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+
+const UserNotFoundPage = () => {
+  return (
+    <div>This user doesn&apos;t exist.</div>
+  )
+}
+
+export default UserNotFoundPage
